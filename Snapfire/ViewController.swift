2009374ae6abd28314704cf8ba4b696f83433fe8
Reza <reza.daneshvar@majority.com>
2025-05-11 19:42:05 +0200

    1. Initialize the iOS project

---
+++ Snapfire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snapfire
+//
+//  Created by Reza on 2025-05-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
